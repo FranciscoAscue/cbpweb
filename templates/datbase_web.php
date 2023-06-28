640ba655bb2bbbd6f5bdfc0e10569606dac55582
Francisco Ascue Orosco <57104899+FranciscoAscue@users.noreply.github.com>
2023-06-28 00:30:31 -0500

    Update datbase_web.php

--- templates/datbase_web.php
+++ templates/datbase_web.php
@@ -1,9 +1,9 @@
 <?php
 // detalles
 $host = 'localhost';
-$dbname = "cbp";//'cbperuo1_web';
-$username = "root";//'cbperuo1_request';
-$password = "123a456"; //'3UR6uK!f13h+' ;
+$dbname = "cbp";
+$username = "root";
+$password = "123a456";
 
 try {
     $conn_web = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
